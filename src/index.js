2fcf1e49d5d342963a56867281d26e3aa477bc94
Sharan Dhanala <sharanraj.d@gmail.com>
2020-02-22 13:19:53 +1300

    Replace loading text with text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react';
 import {Cloudinary} from 'cloudinary-core';
 import defaultStyle from './style';
 import PropTypes from 'prop-types';
-import {View, ImageBackground, PixelRatio, Dimensions, Text} from 'react-native';
+import {
+    View,
+    ImageBackground,
+    PixelRatio,
+    Dimensions,
+    Text,
+    Image
+} from 'react-native';
 
 export default class CloudinaryImage extends Component {
     static propTypes = {
@@ -14,7 +21,7 @@ export default class CloudinaryImage extends Component {
         height: PropTypes.number,
         style: PropTypes.number,
         borderRadius: PropTypes.number
-    }
+    };
     static defaultProps = {
         cloudName: ' ',
         options: {
@@ -27,8 +34,8 @@ export default class CloudinaryImage extends Component {
         style: {},
         sideMargin: 0,
         borderRadius: 0
-    }
-    constructor(props){
+    };
+    constructor(props) {
         super(props);
         this.onLoad = () => this.loaded();
         this.onError = () => this.error();
@@ -40,14 +47,11 @@ export default class CloudinaryImage extends Component {
     getImageUri(width, computedHeight) {
         const {imageId, cloudName, options} = this.props;
         const cloudinary = Cloudinary.new({cloud_name: cloudName});
-        return cloudinary.url(
-            imageId,
-            {
-                ...options,
-                width: PixelRatio.getPixelSizeForLayoutSize(width),
-                height: PixelRatio.getPixelSizeForLayoutSize(computedHeight)
-            }
-        );
+        return cloudinary.url(imageId, {
+            ...options,
+            width: PixelRatio.getPixelSizeForLayoutSize(width),
+            height: PixelRatio.getPixelSizeForLayoutSize(computedHeight)
+        });
     }
     loaded() {
         this.setState({loaded: true});
@@ -59,24 +63,41 @@ export default class CloudinaryImage extends Component {
         const {loaded, error} = this.state;
         const {sideMargin, width, height, style, borderRadius} = this.props;
         const layout = Dimensions.get('window');
-        const computedWidth = width - (2 * sideMargin);
+        const computedWidth = width - 2 * sideMargin;
         const ratio = computedWidth / layout.width;
         const computedHeight = height / ratio;
-        const imageDimensions = {width: layout.width - (2 * sideMargin), height: computedHeight};
-        if(error) return <Text style={defaultStyle.errorText}>{'Error while loading image'}</Text>;
+        const imageDimensions = {
+            width: layout.width - 2 * sideMargin,
+            height: computedHeight
+        };
+        if (error)
+            return (
+                <Text style={defaultStyle.errorText}>
+                    {'Error while loading image'}
+                </Text>
+            );
         return (
-            <ImageBackground style={[imageDimensions, style]}
-                   borderRadius={borderRadius}
-                   onLoad={this.onLoad}
-                   onError={this.onError}
-                   source={{
-                       uri: this.getImageUri(layout.width, computedHeight),
-                       cache: 'force-cache'
-                   }}>
-                   {!loaded && <View style={defaultStyle.loadingContainer}>
-                       <Text style={defaultStyle.loadingText}>{'Loading...'}</Text>
-                   </View>}
+            <ImageBackground
+                style={[imageDimensions, style]}
+                borderRadius={borderRadius}
+                onLoad={this.onLoad}
+                onError={this.onError}
+                source={{
+                    uri: this.getImageUri(layout.width, computedHeight),
+                    cache: 'force-cache'
+                }}
+            >
+                {!loaded && (
+                    <View style={defaultStyle.loadingContainer}>
+                        <Text style={defaultStyle.loadingText}>
+                            {'Loading...'}
+                        </Text>
+                        <Image
+                            source={require('../Images/CrankworxRotorua.png')}
+                        />
+                    </View>
+                )}
             </ImageBackground>
-        )
+        );
     }
 }
